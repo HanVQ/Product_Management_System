9ed4f40b7294ec72ad1d4fb369774a846d8701ea
Paul Nguyen <paul.nguyen@cebupacificair.com>
2026-01-05 11:37:44 +0700

    refactor BE/routes

--- BE/routes/customers.js
+++ BE/routes/customers.js
@@ -8,7 +8,7 @@ router.use(authenticate, requireRole('admin'));
 
 router.get('/', (req, res) => customerController.listCustomer(req, res));
 router.post('/', (req, res) => customerController.createCustomer(req, res));
-router.post('/bulk', (req, res) => customerController.createManyCustomers(req, res));
+router.post('/bulk', (req, res) => customerController.createManyCustomer(req, res));
 router.get('/:id', (req, res) => customerController.getCustomer(req, res));
 router.put('/:id', (req, res) => customerController.updateCustomer(req, res));
 router.delete('/:id', (req, res) => customerController.removeCustomer(req, res));
