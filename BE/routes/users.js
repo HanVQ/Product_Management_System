fdc931fb7b1d5118da754ee5e118d4f25e53d68e
Paul Nguyen <paul.nguyen@cebupacificair.com>
2026-01-05 11:37:44 +0700

    refactor users routes

--- BE/routes/users.js
+++ BE/routes/users.js
@@ -6,11 +6,11 @@ const { authenticate, requireRole } = require('../middleware/auth');
 // All routes protected and require admin role
 router.use(authenticate, requireRole('admin'));
 
-router.get('/', (req, res) => userController.list(req, res));
-router.post('/', (req, res) => userController.create(req, res));
-router.post('/bulk', (req, res) => userController.createMany(req, res));
-router.get('/:id', (req, res) => userController.get(req, res));
-router.put('/:id', (req, res) => userController.update(req, res));
-router.delete('/:id', (req, res) => userController.remove(req, res));
+router.get('/', (req, res) => userController.listUser(req, res));
+router.post('/', (req, res) => userController.createUser(req, res));
+router.post('/bulk', (req, res) => userController.createManyUser(req, res));
+router.get('/:id', (req, res) => userController.getUser(req, res));
+router.put('/:id', (req, res) => userController.updateUser(req, res));
+router.delete('/:id', (req, res) => userController.removeUser(req, res));
 
 module.exports = router;
